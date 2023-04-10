4fc0631212e53fc8add36cf8e2cd0e78440a4246
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2023-04-10 11:57:06 +0600

    server setup and post user

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrj5k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db('dreamJob');
+    const userCollection = db.collection('user');
+    
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+
+      const result = await userCollection.insertOne(user);
+
+      res.send(result);
+    });  
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
